a626e2baa60495476769eb1fdf948e3208782272
Sudipta Ranjan Sahoo <122822255+SudiptaRS@users.noreply.github.com>
2023-03-12 22:29:38 +0530

    Update Question5.c

--- Question5.c
+++ Question5.c
@@ -4,7 +4,7 @@ float r;
 printf("Enter the radius of the circle:");
 scanf("%f",&r);
 printf("The diameter is : %f\n",2*r);
-const pi=3.14;
+float pi=3.14;
 float c=2*pi*r;
 printf("The circumference is : %f\n",c);
 float a=pi*r*r;
